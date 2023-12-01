8d9c34765022729d2afdb1637b35f0e7dd8cf050
ywgoofy <S10257300@connect.np.edu.sg>
2023-12-01 23:48:09 +0800

    finalised

--- script.js
+++ script.js
@@ -38,24 +38,3 @@ function removeHoverEffect() {
 }
 
 
-function toggleDropdown() {
-    var dropdownContent = document.getElementById("socialMediaContent");
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.w3-button')) {
-        var dropdowns = document.getElementsByClassName("w3-dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.style.display === "block") {
-                openDropdown.style.display = "none";
-            }
-        }
-    }
-}
